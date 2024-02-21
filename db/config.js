2f88b07a417dbe9154b9cb44de02c2878d25ada9
preetamimg <99808975+preetamimg@users.noreply.github.com>
2024-02-21 13:27:16 +0530

    CHANGES IN CONFIG FILE

--- db/config.js
+++ db/config.js
@@ -1,6 +1,15 @@
 import mongoose from 'mongoose'
+import dotenv from 'dotenv'
 
-mongoose.connect("mongodb+srv://preetamimg:WRrhwFiAoyGRY5Zn@cluster0.wwwheqi.mongodb.net/?retryWrites=true&w=majority", 
+dotenv.config()
+
+
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}.mongodb.net/?retryWrites=true&w=majority`, 
 {useNewUrlParser: true, useUnifiedTopology: true})
+
+
+// mongoose.connect("mongodb+srv://preetamimg:WRrhwFiAoyGRY5Zn@cluster0.wwwheqi.mongodb.net/?retryWrites=true&w=majority", 
+// {useNewUrlParser: true, useUnifiedTopology: true})
+
 
 // mongoose.connect('mongodb://127.0.0.1:27017/blog')
